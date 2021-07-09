935af9a83ab5cd7fbecfa1b09d51d48a4b37b15e
StevenPlai <stevenrplai@gmail.com>
2021-07-09 00:39:30 -0500

    Added train & test functionality

--- Submissions/SIS_RCode/XGBoost.R
+++ Submissions/SIS_RCode/XGBoost.R
@@ -2,6 +2,7 @@ library(tidyverse)
 library(RCurl)
 library(fastDummies)
 library(xgboost)
+library(ggplot2)
 
 #load in the files
 pbp_raw <- getURL("https://raw.githubusercontent.com/jackp01k/AnalyticsChallenge2021/main/Data/PlayByPlay.csv")
@@ -66,7 +67,7 @@ pos_data <- splayers %>% select(GameID, EventID, OnFieldPosition)
 pbp_pos_data <- left_join(model_data, pos_data, by=c("GameID", "EventID")) %>%
   dummy_cols(select_columns = "OnFieldPosition") 
 
-names <- data.frame(n = colnames(pbp_pos_data)) %>% slice(45:59)
+names <- data.frame(n = colnames(pbp_pos_data)) %>% dplyr::slice(45:59)
 
 pbp_pos_data <- pbp_pos_data %>% group_by(GameID, EventID) %>%
   summarise(B = sum(OnFieldPosition_B),
@@ -84,16 +85,54 @@ pbp_route_data <- left_join(model_data, route_data, by=c("GameID", "EventID")) %
 pbp_route_data <- pbp_route_data %>% group_by(GameID, EventID) %>%
   summarise(across(Route_Angle:Route_Whip, sum))
 
-model_data <- model_data %>% left_join(pbp_route_data, by=c("GameID", "EventID")) %>%
-  select(-GameID, -EventID)
+model_Data <- model_data %>% left_join(pbp_route_data, by=c("GameID", "EventID")) %>%
+  select(-GameID, -EventID, -Route_NULL)
 
-model_label <- as.matrix(model_data %>% select(EPA))
-model_data <- as.matrix(model_data %>% select(-EPA))
+model_label <- as.matrix(model_Data %>% select(EPA))
+model_data <- as.matrix(model_Data %>% select(-EPA))
 
 model_matrix <- xgb.DMatrix(data = model_data, label = model_label)
 
-model <- xgboost(data = model_matrix, nrounds = 2000)
+model <- xgboost(data = model_matrix, nrounds = 100)
 
 importance_matrix <- xgb.importance(model = model)
 print(importance_matrix)
 xgb.plot.importance(importance_matrix = importance_matrix)
+
+#Test Train
+
+ind <- sample(c(TRUE, FALSE), nrow(model_Data), replace=TRUE, prob=c(0.5, 0.5))
+test <- model_Data[ind, ]
+train <- model_Data[!ind, ]
+
+tr_data <- as.matrix(train %>% select(-EPA))
+tr_label <- as.matrix(train %>% select(EPA))
+
+te_data <- as.matrix(test %>% select(-EPA))
+te_label <- as.matrix(test %>% select(EPA))
+
+train_data <- xgb.DMatrix(data = tr_data, label = tr_label)
+
+train_model <- xgboost(data = train_data, nrounds = 100)
+
+pred <- predict(train_model, te_data)
+pd <- test
+pd$Pred <- pred
+err <- mean(abs(test$EPA - pred))
+
+importance_matrix <- xgb.importance(model = train_model)
+print(importance_matrix)
+xgb.plot.importance(importance_matrix = importance_matrix)
+
+#Plot Error
+
+pd <- pd %>% select(TimeLeft, EPA, Pred)
+
+pda <- pd %>% select(TimeLeft, EPA) %>% mutate(Type = "A")
+pdb <- pd %>% select(TimeLeft, EPA = Pred) %>% mutate(Type = "B")
+
+pd <- bind_rows(pda, pdb)
+
+ggplot(data = pd, aes(x = TimeLeft, y = EPA, color = Type)) +
+  geom_point()
+
